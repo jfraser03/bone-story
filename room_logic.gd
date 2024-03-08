11025ac7968d689078bafd47b8ccbca3cb8ae2c7
Jacob Fraser <jakebfraser@gmail.com>
2024-03-08 15:24:53 -0500

    Began work on scrolling textbox.

--- room_logic.gd
+++ room_logic.gd
@@ -7,4 +7,29 @@ func _ready():
 	Bonesly.interacted.connect(text_box_message)
 
 func text_box_message(message):
-	UI.set_message(message)
+	var pages = create_pages(message)
+	print(pages)
+	#UI.set_message(pages)
+	
+func create_pages(message):
+	var pages = []
+	var char_limit = 50
+	var words = message.split(" ")
+	
+	var current_page = ""
+	var current_length = 0
+	for word in words:
+		if current_length + word.length() + 1 > char_limit:
+			# Finish this page
+			pages.append(current_page.strip_edges(true))
+			# Start a new page
+			current_page = word + " "
+			current_length = word.length() + 1
+		else:
+			# Add word to current page
+			current_page += word + " "
+			current_length += word.length() + 1
+	if current_page != "":
+		pages.append(current_page.strip_edges(true))
+	
+	return pages
--- ui.gd
+++ ui.gd
@@ -3,16 +3,21 @@ extends Control
 @onready var label = $Panel/Label
 @onready var panel = $Panel
 
+var character_limit = 84
+
 signal textbox_finished
 
 func set_message(message):
+	var pages = message.length() / character_limit
+	
+	
+	
 	panel.set_visible(true)
-	label.set_visible_characters(0)
+	label.set_visible_ratio(0)
 	label.set_text(message)
 	
 	var tween = get_tree().create_tween()
-	var chars = label.text.length()
-	tween.tween_property(label, "visible_characters", chars, 1)
+	tween.tween_property(label, "visible_ratio", 1.0, 1.5)
 	
 func _on_tween_completed():
 	await get_tree().create_timer(2).timeout
